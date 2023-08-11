0a4e6b943dd90f1c03bbdd2299bd0f2b063d4771
Béryl HOUESSOU <berylhoe@gmail.com>
2023-08-11 14:20:08 +0100

    Add animation to menu

--- app/page.tsx
+++ app/page.tsx
@@ -164,7 +164,7 @@ export default function Home() {
   }, [isIntersectingExperience]);
 
   return (
-    <div onMouseMove={onMouseMove} className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 md:flex md:justify-end md:relative">
+    <div onMouseMove={onMouseMove} className="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900 mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 md:flex md:justify-end md:relative">
       <div className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute" style={{ background: `radial-gradient(600px at ${cursorPosition.left}px ${cursorPosition.top}px, rgba(29, 78, 216, 0.15), transparent 80%)` }}></div>
       <div className="lg:flex lg:justify-between lg:gap-4"></div>
 
